f6540eef7168781893223a19c9f701e6f132a7fd
brianna audain <briannaaudain2@outlook.com>
2022-03-26 13:39:01 -0500

    initial commit

---
+++ OperationExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OperationExercise 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+
+            int diff = a - b;
+
+            int product = a * b;
+
+            double quotient = a / b;
+
+            int remainder = a % b;
+
+            Console.WriteLine($"{a} + {b} is {sum}");
+            Console.WriteLine($"{a} - {b} is {diff}");
+            Console.WriteLine($"{a} * {b} is {product}");
+            Console.WriteLine($"{a} / {b} is {quotient} remainder is {remainder}");
+
+
+        }
+    }
+}
